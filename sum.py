6f13cbe44d8b0e0b186a97fc5fea88d8e13c7e62
Souro Biswas <hopepope911@gmail.com>
2022-12-31 00:12:34 +0530

    initial commit

---
+++ sum.py
@@ -0,0 +1,12 @@
+a=int(input("enter a number:"))
+b=int(input("enter another number:"))
+
+print("what you want to do?")
+print("for addition press:c")
+print("for substraction press:d")
+
+x=input("what you want to do?")
+if x=="c":
+    print(a+b)
+if x=="d":
+    print(a-b)
